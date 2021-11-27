bc1cf870f27d67c144a5f518cace76707e4e6070
code-bebop <woonj2@gmail.com>
2021-11-27 22:13:19 +0900

    ApolloClient로 API 연결
    Home route 에서 movie API를 불러오게 했다.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,25 @@
-export default () => "Home";
+import { useQuery } from "@apollo/client";
+import { gql } from "graphql-tag";
+
+const GET_MOVIES = gql`
+    {
+        movies{
+            id
+            medium_cover_image
+        }
+    }
+`
+
+const HomeComponent = () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    console.log(loading, error, data);
+    return (
+        <div>
+            {loading && <p>loading</p>}
+            {!loading && data.movies && 
+            data.movies.map(({ id }) => <p key={id}>{id}</p>)}
+        </div>   
+    )
+}
+
+export default HomeComponent;
